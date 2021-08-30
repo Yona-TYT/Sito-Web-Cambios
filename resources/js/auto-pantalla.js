c2bb3c589a8abb8586e1591418d72629b7e0dac7
Yonatan <yonatan.el.amigo@gmail.com>
2021-08-30 19:37:48 -0400

    Test

--- resources/js/auto-pantalla.js
+++ resources/js/auto-pantalla.js
@@ -7,28 +7,31 @@ function check_windows_siz() {
 	var objref = document.body
 	var font_siz = getComputedStyle(objref).getPropertyValue("--siz-text");
 
-	if(ancho <= 480){
-		console.log(+ancho+"  " +font_siz);
-		//objref.style.setProperty("--siz-text", '100%');
-		gl_mobil = true;
-		int_trans();
+	if(!gl_mobil){
+		if(ancho <= 480){
+			console.log(+ancho+"  " +font_siz);
+			//objref.style.setProperty("--siz-text", '100%');
+			gl_mobil = true;
+			int_trans();
+		}
+		else if(ancho <= 768){
+			console.log(+ancho+"  " +font_siz);
+			//objref.style.setProperty("--siz-text", '100%');
+			gl_mobil = true;
+			int_trans();
+		}
+		else if(ancho <= 1024){
+			console.log(+ancho+"  " +font_siz);
+			//objref.style.setProperty("--siz-text", '100%');
+			gl_mobil = true;
+			int_trans();
+		}
+
 	}
-	else if(ancho <= 768){
-		console.log(+ancho+"  " +font_siz);
-		//objref.style.setProperty("--siz-text", '100%');
-		gl_mobil = true;
-		int_trans();
-	}
-	else if(ancho <= 1024){
-		console.log(+ancho+"  " +font_siz);
-		//objref.style.setProperty("--siz-text", '100%');
-		gl_mobil = true;
-		int_trans();
-	}
-	else {
-		console.log(+ancho+"  " +font_siz);
-		//objref.style.setProperty("--siz-text", '100%');
-		gl_mobil = false;
-		int_trans();
+	else if(ancho >= 1024) {
+			console.log(+ancho+"  " +font_siz);
+			//objref.style.setProperty("--siz-text", '100%');
+			gl_mobil = false;
+			int_trans();
 	}
 }
