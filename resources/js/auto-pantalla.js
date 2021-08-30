7030bbf6f7388777970a1f107846da52015daa18
Yonatan <yonatan.el.amigo@gmail.com>
2021-08-30 17:22:43 -0400

    Test

--- resources/js/auto-pantalla.js
+++ resources/js/auto-pantalla.js
@@ -6,20 +6,29 @@ function check_windows_siz() {
 
 	var objref = document.body
 	var font_siz = getComputedStyle(objref).getPropertyValue("--siz-text");
-alert(ancho);
+
 	if(ancho <= 480){
 		console.log(+ancho+"  " +font_siz);
 		objref.style.setProperty("--siz-text", '30%');
 		gl_mobil = true;
+		int_trans();
 	}
 	else if(ancho <= 768){
 		console.log(+ancho+"  " +font_siz);
 		objref.style.setProperty("--siz-text", '60%');
 		gl_mobil = true;
+		int_trans();
+	}
+	else if(ancho <= 1024){
+		console.log(+ancho+"  " +font_siz);
+		objref.style.setProperty("--siz-text", '90%');
+		gl_mobil = true;
+		int_trans();
 	}
 	else {
 		console.log(+ancho+"  " +font_siz);
 		objref.style.setProperty("--siz-text", '100%');
 		gl_mobil = false;
+		int_trans();
 	}
 }
